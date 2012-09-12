0e035a4e04dd9eac849114c87bab22d132040109
Samuel Croset <samuel.croset@gmail.com>
2012-09-13 00:26:12 +0100

    shipping away!

--- src/main/java/uk/ac/ebi/brain/core/Brain.java
+++ src/main/java/uk/ac/ebi/brain/core/Brain.java
@@ -3,23 +3,38 @@
  */
 package uk.ac.ebi.brain.core;
 
+import java.io.File;
+import java.util.Set;
+
+import org.coode.owlapi.manchesterowlsyntax.ManchesterOWLSyntaxEditorParser;
+import org.coode.owlapi.manchesterowlsyntax.ManchesterOWLSyntaxOntologyFormat;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.expression.OWLEntityChecker;
+import org.semanticweb.owlapi.expression.ParserException;
+import org.semanticweb.owlapi.expression.ShortFormEntityChecker;
 import org.semanticweb.owlapi.model.AddAxiom;
 import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLDeclarationAxiom;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyFormat;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+import org.semanticweb.owlapi.model.OWLTransitiveObjectPropertyAxiom;
 import org.semanticweb.owlapi.model.PrefixManager;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.util.BidirectionalShortFormProvider;
+import org.semanticweb.owlapi.util.BidirectionalShortFormProviderAdapter;
 import org.semanticweb.owlapi.util.DefaultPrefixManager;
 import org.semanticweb.owlapi.util.ShortFormProvider;
+import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -105,6 +120,7 @@ public class Brain {
      * 
      */
     public Brain(String prefix) throws OWLOntologyCreationException{
+	//TODO check that the string finishes wiith / or #
 	this.manager = OWLManager.createOWLOntologyManager();
 	this.factory = manager.getOWLDataFactory();
 	this.ontology = manager.createOntology(IRI.create(prefix));
@@ -116,53 +132,33 @@ public class Brain {
      * @return 
      * @throws ExistingClassException 
      */
-    public OWLClass addOWLClass(String className) throws ExistingClassException {
-	
-	//TODO check if expression if yes --> error
-	
-	if(this.ontology.containsClassInSignature(IRI.create(this.prefixManager.getDefaultPrefix() + className))){
-	    throw new ExistingClassException("The class '"+ className +"' already exists in the ontology.");
-	}
+    public OWLClass addOWLClass(String className) {
+	//TODO throw an error if classes exists already
 	OWLClass owlClass = this.factory.getOWLClass(className, this.prefixManager);
 	OWLDeclarationAxiom declarationAxiom = this.factory.getOWLDeclarationAxiom(owlClass);
 	manager.addAxiom(this.ontology, declarationAxiom);
+	updateShorForms();
 	return owlClass;
     }
 
+    public OWLObjectProperty addOWLObjectProperty(String propName) {
+	//TODO throw an error if classes exists already
+	OWLObjectProperty owlprop = this.factory.getOWLObjectProperty(propName, this.prefixManager);
+	OWLDeclarationAxiom declarationAxiom = this.factory.getOWLDeclarationAxiom(owlprop);
+	manager.addAxiom(this.ontology, declarationAxiom);
+	updateShorForms();
+	return owlprop;
+    }
+
+
     /**
-     * @param string
-     * @param string2
-     * @throws ExistingClassException 
+     * 
      */
-    public void subClassOf(String subClassName, String superClassName) {
-
-	OWLClass subClass = null;
-	try {
-	    subClass = this.getOWLClass(subClassName);
-	} catch (NonExistingEntityException e) {
-	    try {
-		//TODO NAN !
-		subClass = this.addOWLClass(subClassName);
-	    } catch (ExistingClassException e1) {
-		e1.printStackTrace();
-	    }
-	}
-
-	OWLClass superClass = null;
-
-	try {
-	    superClass = this.getOWLClass(superClassName);
-	} catch (NonExistingEntityException e) {
-	    try {
-		superClass = this.addOWLClass(superClassName);
-	    } catch (ExistingClassException e1) {
-		e1.printStackTrace();
-	    }
-	}
-
-	OWLAxiom axiom = factory.getOWLSubClassOfAxiom(subClass, superClass);
-	AddAxiom addAxiom = new AddAxiom(ontology, axiom);
-	manager.applyChange(addAxiom);
+    private void updateShorForms() {
+	this.shortFormProvider = new SimpleShortFormProvider();
+	Set<OWLOntology> importsClosure = this.ontology.getImportsClosure();
+	this.bidiShortFormProvider = new BidirectionalShortFormProviderAdapter(this.manager, importsClosure, this.shortFormProvider);
+	this.entityChecker = new ShortFormEntityChecker(this.bidiShortFormProvider);
     }
 
     /**
@@ -171,12 +167,91 @@ public class Brain {
      * @throws NonExistingEntityException 
      */
     public OWLClass getOWLClass(String className) throws NonExistingEntityException {
-
 	if(this.ontology.containsClassInSignature(IRI.create(this.prefixManager.getDefaultPrefix() + className))){
 	    return this.factory.getOWLClass(className, this.prefixManager);
 	}else{
 	    throw new NonExistingClassException("The entity '"+ className +"' is not an OWL class");
 	}
     }
+
+
+    /**
+     * @param string
+     * @param string2
+     * @throws ParserException 
+     * @throws NonExistingEntityException 
+     * @throws ExistingClassException 
+     */
+    public void subClassOf(String subClassName, String superClassName) throws ParserException {
+	OWLClassExpression subClassExpression = parseClassExpression(subClassName);
+	OWLClassExpression superClassExpression = parseClassExpression(superClassName);
+	OWLSubClassOfAxiom subClassAxiom = factory.getOWLSubClassOfAxiom(subClassExpression, superClassExpression);
+	AddAxiom addAx = new AddAxiom(ontology, subClassAxiom);
+	manager.applyChange(addAx);
+    }
+
+    public void makeTransitive(String propertyExpression) {
+	// TODO Auto-generated method stub
+	OWLObjectPropertyExpression expression = parseObjectPropertyExpression(propertyExpression);
+	OWLTransitiveObjectPropertyAxiom axiom = factory.getOWLTransitiveObjectPropertyAxiom(expression);
+	AddAxiom addAx = new AddAxiom(ontology, axiom);
+	manager.applyChange(addAx);
+    }
+
+    private OWLObjectPropertyExpression parseObjectPropertyExpression(String expression) {
+	ManchesterOWLSyntaxEditorParser parser = new ManchesterOWLSyntaxEditorParser(factory, expression);
+	parser.setDefaultOntology(ontology);
+	parser.setOWLEntityChecker(entityChecker);
+	OWLObjectPropertyExpression owlExpression = null;
+	try {
+	    owlExpression = parser.parseObjectPropertyExpression();
+	} catch (ParserException e) {
+	    // TODO Auto-generated catch block
+	    //TODO printing error perso
+	    e.printStackTrace();
+	}
+
+	return owlExpression;
+    }
+
+
+    /**
+     * Converts a string into an OWLExpression. If a problem is encountered, an error is thrown which can be catched-up
+     * in order to know more about the error.
+     * @param expression
+     * @return owlExpression
+     * @throws ParserException 
+     */
+    private OWLClassExpression parseClassExpression(String expression) {
+	ManchesterOWLSyntaxEditorParser parser = new ManchesterOWLSyntaxEditorParser(factory, expression);
+	parser.setDefaultOntology(ontology);
+	parser.setOWLEntityChecker(entityChecker);
+	OWLClassExpression owlExpression = null;
+	try {
+	    owlExpression = parser.parseClassExpression();
+	} catch (ParserException e) {
+	    // TODO Auto-generated catch block
+	    //TODO printing error perso
+	    e.printStackTrace();
+	}
+
+	return owlExpression;
+    }
+
+    /**
+     * @param string
+     * @throws OWLOntologyStorageException 
+     */
+    public void save(String path) throws OWLOntologyStorageException {
+	// TODO Auto-generated method stub
+	File file = new File(path);
+	OWLOntologyFormat format = manager.getOntologyFormat(this.ontology);
+	ManchesterOWLSyntaxOntologyFormat manSyntaxFormat = new ManchesterOWLSyntaxOntologyFormat();
+	if (format.isPrefixOWLOntologyFormat()) {
+	    manSyntaxFormat.copyPrefixesFrom(format.asPrefixOWLOntologyFormat());
+	}
+	manager.saveOntology(this.ontology, manSyntaxFormat, IRI.create(file));
+    }
+
 
 }
--- src/test/java/uk/ac/ebi/BrainTest.java
+++ src/test/java/uk/ac/ebi/BrainTest.java
@@ -7,9 +7,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.semanticweb.owlapi.expression.ParserException;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 
 import uk.ac.ebi.brain.core.Brain;
 import uk.ac.ebi.brain.error.ExistingClassException;
@@ -25,7 +27,7 @@ public class BrainTest {
 
     @Before
     public void bootstrap() throws OWLOntologyCreationException {
-	brain = new Brain("src/test/resources/dev.owl");
+	brain = new Brain("http://www.example.org#");
     }
 
     @Test
@@ -34,27 +36,52 @@ public class BrainTest {
 	assertEquals(true, brain.getOntology().containsClassInSignature(IRI.create(brain.getPrefixManager().getDefaultPrefix() + "A")));
     }
 
-    @Test(expected = ExistingClassException.class)
-    public void addExistingClassTest() throws ExistingClassException{
-	brain.addOWLClass("A");
-	assertEquals(true, brain.getOntology().containsClassInSignature(IRI.create(brain.getPrefixManager().getDefaultPrefix() + "A")));
-	brain.addOWLClass("A");
-    }
-    
     @Test
-    public void getOwlClassTest() throws ExistingClassException, NonExistingEntityException {
+    public void addExistingClassTest() throws ExistingClassException{
+	OWLClass owlClass1 = brain.addOWLClass("A");
+	assertEquals(true, brain.getOntology().containsClassInSignature(IRI.create(brain.getPrefixManager().getDefaultPrefix() + "A")));
+	OWLClass owlClass2 = brain.addOWLClass("A");
+	assertEquals(owlClass1, owlClass2);
+    }
+
+    @Test
+    public void getOwlClassTest() throws NonExistingEntityException {
 	brain.addOWLClass("A");
 	OWLClass classA = brain.getOWLClass("A");
 	assertNotNull(classA);
     }
     
-    @Test
-    public void addSubClassOfTest() throws NonExistingEntityException{
-	brain.subClassOf("A", "B");
-	assertNotNull(brain.getOWLClass("A"));
-	
-	brain.subClassOf("A", "part-of some B");
+    @Test(expected = NonExistingEntityException.class)
+    public void getNonExsistingOwlClassTest() throws NonExistingEntityException {
+	@SuppressWarnings("unused")
+	OWLClass classA = brain.getOWLClass("A");
     }
+
+    @Test
+    public void subClassOfTest() throws ParserException{
+	brain.addOWLClass("A");
+	brain.addOWLClass("B");
+	brain.subClassOf("A", "B");
+    }
+    
+    @Test
+    public void saveOntology() throws OWLOntologyStorageException, ParserException{
+	brain.addOWLClass("A");
+	brain.addOWLClass("B");
+	brain.subClassOf("A", "B");
+	brain.save("src/test/resources/output.owl");
+    }
+    
+    @Test
+    public void testProp() throws OWLOntologyStorageException, ParserException {
+	brain.addOWLClass("A");
+	brain.addOWLClass("B");
+	brain.addOWLObjectProperty("part-of");
+	brain.makeTransitive("part-of");
+	brain.subClassOf("A", "part-of some B");
+	brain.save("src/test/resources/output.owl");
+    }
+
 
 
 }
