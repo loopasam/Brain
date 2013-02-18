9a7691ff883f5989aa0dbacf5e77b1871cd21407
loopasam <samuel.croset@gmail.com>
2013-02-18 10:41:27 +0000

    licencing updated
    Signed-off-by: loopasam <samuel.croset@gmail.com>

--- src/main/java/uk/ac/ebi/brain/core/Brain.java
+++ src/main/java/uk/ac/ebi/brain/core/Brain.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright [2013] EMBL - European Bioinformatics Institute
+ * Licensed under the Apache License, Version 2.0 (the 
+ * "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of 
+ * the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+ * KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations 
+ * under the License.
+ */
 package uk.ac.ebi.brain.core;
 
 import java.io.File;
@@ -162,13 +176,6 @@ public class Brain {
 		this.entityChecker = new ShortFormEntityChecker(this.bidiShortFormProvider);
 
 		Logger.getLogger("org.semanticweb.elk").setLevel(Level.OFF);
-
-		//TODO test
-//		this.configuration = new ElkReasonerConfiguration();
-//		this.configuration.getElkConfiguration().setParameter(ReasonerConfiguration.NUM_OF_WORKING_THREADS, Integer.toString(4));
-//		this.reasonerFactory = new ElkReasonerFactory();
-//		this.reasoner = this.getReasonerFactory().createReasoner(this.ontology, this.configuration);
-		//TODO end test
 		
 		this.reasonerFactory = new ElkReasonerFactory();
 		this.reasoner = this.getReasonerFactory().createReasoner(this.ontology);
